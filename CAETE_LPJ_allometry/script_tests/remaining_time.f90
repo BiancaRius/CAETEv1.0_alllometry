ef7635bfff5fc25ed3c7b03d8ac27ea6430707c8
Bianca Rius <biancafaziorius@gmail.com>
2022-01-03 12:05:32 -0300

    taking off allocatable scheme

--- CAETE_LPJ_allometry/script_tests/remaining_time.f90
+++ CAETE_LPJ_allometry/script_tests/remaining_time.f90
@@ -2,32 +2,32 @@ program self_thinning
 
     ! ================= VARIABLES TO USE DECLARATION ===================== !
     integer :: j,k
-    integer, parameter :: npls = 20 !40 !20
-    integer, parameter :: time = 500
-    real, dimension(npls), allocatable :: lai (:) !Leaf Area Index (m2/m2)
-    real, dimension(npls), allocatable :: diam (:) !Tree diameter in m. (Smith et al., 2001 - Supplementary)
-    real, dimension(npls), allocatable :: crown_area (:) !Tree crown area (m2) (Sitch et al., 2003)
+    integer, parameter :: npls = 5 !40 !20
+    integer, parameter :: time = 5
+    real, dimension(npls) :: lai !Leaf Area Index (m2/m2)
+    real, dimension(npls) :: diam !Tree diameter in m. (Smith et al., 2001 - Supplementary)
+    real, dimension(npls) :: crown_area !Tree crown area (m2) (Sitch et al., 2003)
     real, dimension(npls) :: dens_1 != 2  !densidade de indivíduos em cada PLS (0.5 é um número genérico)
     real, dimension(npls) :: dens_2 != 2  !densidade de indivíduos em cada PLS (0.5 é um número genérico)
 
    
     
     ! real, dimension(npls) :: est_pls !establishment for a specific PLS
-    real, allocatable :: FPC_ind (:) !Foliage projective cover for each average individual of a PLS (Stich et al., 2003)
-    real, allocatable :: FPC_pls_1 (:) !Total Foliage projective cover of a PLS (Stich et al., 2003)
-    real, allocatable :: FPC_pls_2 (:) !Total Foliage projective cover of a PLS (Stich et al., 2003)
-    real, allocatable :: FPC_pls (:) !Total Foliage projective cover of a PLS (Stich et al., 2003)
-    real, allocatable :: FPC_grid_t1 (:) !Fractional projective cover for each PLS in time 1 (Sitch et al., 2003)
-    real, allocatable :: FPC_grid_t2 (:) !Fractional projective cover for each PLS in time 2 (Sitch et al., 2003)
-    real, allocatable :: fpc_dec (:) !'decrease FPC' in other words: the excedend of FPC in eac year [LPJ-GUESS - Phillip's video]
-    real, allocatable :: fpc_dec_prop (:) !proportion of the decrease of a PLS
-    real, allocatable :: mort (:) !equivalente ao 'mort_shade' no LPJ-GUESS [Phillip's video]
-    real, allocatable :: mort_greff (:) ! motallity from growth efficiency (Sitch et al 2003)
-    real, allocatable :: greff (:) ! motallity from growth efficiency (Sitch et al 2003)
-    real, allocatable :: remaining (:) !taxa de redução
-    real, allocatable :: FPC_inc (:)
-    real, allocatable :: FPC_inc_cont (:)
-    real, allocatable :: carbon_increment (:) ! used to calculate mort greff (Sitch et al 2003)
+    real, dimension(npls) :: FPC_ind !Foliage projective cover for each average individual of a PLS (Stich et al., 2003)
+    real, dimension(npls) :: FPC_pls_1  !Total Foliage projective cover of a PLS (Stich et al., 2003)
+    real, dimension(npls) :: FPC_pls_2  !Total Foliage projective cover of a PLS (Stich et al., 2003)
+    real, dimension(npls) :: FPC_pls  !Total Foliage projective cover of a PLS (Stich et al., 2003)
+    real, dimension(npls) :: FPC_grid_t1  !Fractional projective cover for each PLS in time 1 (Sitch et al., 2003)
+    real, dimension(npls) :: FPC_grid_t2  !Fractional projective cover for each PLS in time 2 (Sitch et al., 2003)
+    real, dimension(npls) :: fpc_dec  !'decrease FPC' in other words: the excedend of FPC in eac year [LPJ-GUESS - Phillip's video]
+    real, dimension(npls) :: fpc_dec_prop  !proportion of the decrease of a PLS
+    real, dimension(npls) :: mort  !equivalente ao 'mort_shade' no LPJ-GUESS [Phillip's video]
+    real, dimension(npls) :: mort_greff  ! motallity from growth efficiency (Sitch et al 2003)
+    real, dimension(npls) :: greff  ! motallity from growth efficiency (Sitch et al 2003)
+    real, dimension(npls) :: remaining  !taxa de redução
+    real, dimension(npls) :: FPC_inc 
+    real, dimension(npls) :: FPC_inc_cont 
+    real, dimension(npls) :: carbon_increment  ! used to calculate mort greff (Sitch et al 2003)
     
     real :: FPC_total_1 = 0.0 !sum of FPC_grid 
     real :: FPC_total_2 = 0.0 !sum of FPC_grid in
@@ -86,28 +86,28 @@ program self_thinning
 
     
     !ARRAYS WITH GENERIC VALUES OF SOME VARIABLES
-    dwood=(/0.24,0.53,0.39,0.32,0.31,0.44,0.66,0.42,0.74,0.39,0.82,0.40,0.26,0.79,0.39,0.52,0.41,0.44,0.86,0.42/)!,&
+    dwood=(/0.24,0.53,0.39,0.32,0.31/)!,0.44,0.66,0.42,0.74,0.39,0.82,0.40,0.26,0.79,0.39,0.52,0.41,0.44,0.86,0.42/)!,&
     ! &0.24,0.53,0.39,0.32,0.31,0.44,0.66,0.42,0.74,0.39,0.82,0.40,0.26,0.79,0.39,0.52,0.41,0.44,0.86,0.42/) !atenção para a unidade
     
     ! dwood=(/0.24,0.53,0.39,0.32,0.31,0.44,0.66,0.42,0.74,0.39,0.82,0.40,0.26,0.79,0.39,0.52,0.41,0.44,0.86,0.42,&
     ! &0.24,0.53,0.39,0.32,0.31,0.44,0.66,0.42,0.74,0.39,0.82,0.40,0.26,0.79,0.39,0.52,0.41,0.44,0.86,0.42/) !atenção para a unidade
 
 
-    cl1_initial=(/.7,1.,0.3,1.6,1.10,1.8,0.3,0.2,0.8,0.84,0.25,1.,0.2,1.7,0.4,.6,.5,.8,0.3,1.8/)!,&
+    cl1_initial=(/.7,1.,0.3,1.6,1.10/)!,1.8,0.3,0.2,0.8,0.84,0.25,1.,0.2,1.7,0.4,.6,.5,.8,0.3,1.8/)!,&
     ! &.7,1.,0.3,1.6,1.10,1.8,0.3,0.2,0.8,0.84,0.25,1.,0.2,1.7,0.4,.6,.5,.8,0.3,1.8/)
 
     ! cl1=(/.7,1.,0.3,1.6,1.10,1.8,0.3,0.2,0.8,0.84,0.25,1.,0.2,1.7,0.4,.6,.5,.8,0.3,1.8,&
     ! &.7,1.,0.3,1.6,1.10,1.8,0.3,0.2,0.8,0.84,0.25,1.,0.2,1.7,0.4,.6,.5,.8,0.3,1.8/)
     
-    spec_leaf=(/0.0153,0.0101,0.0107,0.0112,0.012,0.0141,0.0137,0.0115,0.0122,0.010,0.012,0.011,&
-    &0.013,0.014,0.0112,0.012,0.0141,0.0137,0.0115,0.0122/)! !,0.0153,0.0101,0.0107,0.0112,0.012,&
+    spec_leaf=(/0.0153,0.0101,0.0107,0.0112,0.012/)!,0.0141,0.0137,0.0115,0.0122,0.010,0.012,0.011,&
+   ! &0.013,0.014,0.0112,0.012,0.0141,0.0137,0.0115,0.0122/)! !,0.0153,0.0101,0.0107,0.0112,0.012,&
     ! ! &0.0141,0.0137,0.0115,0.0122,0.010,0.012,0.011,0.013,0.014,0.0112,0.012,0.0141,0.0137,0.0115,0.0122/)
    
     ! spec_leaf=(/0.0153,0.0101,0.0107,0.0112,0.012,0.0141,0.0137,0.0115,0.0122,0.010,0.012,0.011,&
     ! &0.013,0.014,0.0112,0.012,0.0141,0.0137,0.0115,0.0122,0.0153,0.0101,0.0107,0.0112,0.012,&
     ! &0.0141,0.0137,0.0115,0.0122,0.010,0.012,0.011,0.013,0.014,0.0112,0.012,0.0141,0.0137,0.0115,0.0122/)
     
-    cw1_initial=(/30.,22.,34.,28.3,20.2,19.7,27.5,19.5,20.,28.6,24.3,19.3,26.8,22.,18.3,22.,15.,22.6,10.7,21.4/)!,&
+    cw1_initial=(/30.,22.,34.,28.3,20.2/)!,19.7,27.5,19.5,20.,28.6,24.3,19.3,26.8,22.,18.3,22.,15.,22.6,10.7,21.4/)!,&
     !&30.,22.,34.,28.3,20.2,19.7,27.5,19.5,20.,28.6,24.3,19.3,26.8,22.,18.3,22.,15.,22.6,10.7,21.4/)
     
     ! cw1=(/30.,22.,34.,28.3,20.2,19.7,27.5,19.5,20.,28.6,24.3,19.3,26.8,22.,18.3,22.,15.,22.6,10.7,21.4,&
@@ -115,50 +115,31 @@ program self_thinning
     
 
 
-    cr1_initial=(/0.63,0.8,0.9,1.5,1.3,0.9,0.4,1.0,0.56,0.87,0.33,0.97,0.31,0.55,0.2,0.8,0.4,0.66,0.23,1.5/)!,&
+    cr1_initial=(/0.63,0.8,0.9,1.5,1.3/)!,0.9,0.4,1.0,0.56,0.87,0.33,0.97,0.31,0.55,0.2,0.8,0.4,0.66,0.23,1.5/)!,&
     ! !&0.63,0.8,0.9,1.5,1.3,0.9,0.4,1.0,0.56,0.87,0.33,0.97,0.31,0.55,0.2,0.8,0.4,0.66,0.23,1.5/)
 
     ! cr1=(/0.63,0.8,0.9,1.5,1.3,0.9,0.4,1.0,0.56,0.87,0.33,0.97,0.31,0.55,0.2,0.8,0.4,0.66,0.23,1.5,&
     ! &0.63,0.8,0.9,1.5,1.3,0.9,0.4,1.0,0.56,0.87,0.33,0.97,0.31,0.55,0.2,0.8,0.4,0.66,0.23,1.5/)
     
-    npp1 = (/0.5,0.8,1.5,1.2,1.9,1.3,1.7,0.8,0.6,2.0,0.7,1.1,1.9,1.85,1.96,1.77,1.33,1.54,1.62,0.55/)!,&
+    npp1 = (/0.5,0.8,1.5,1.2,1.9/)!,1.3,1.7,0.8,0.6,2.0,0.7,1.1,1.9,1.85,1.96,1.77,1.33,1.54,1.62,0.55/)!,&
     ! &0.5,0.8,1.5,1.2,1.9,1.3,1.7,0.8,0.6,2.0,0.7,1.1,1.9,1.85,1.96,1.77,1.33,1.54,1.62,0.55/)
 
     ! npp1 = (/0.5,0.8,1.5,1.2,1.9,1.3,1.7,0.8,0.6,2.0,0.7,1.1,1.9,1.85,1.96,1.77,1.33,1.54,1.62,0.55,&
     ! &0.5,0.8,1.5,1.2,1.9,1.3,1.7,0.8,0.6,2.0,0.7,1.1,1.9,1.85,1.96,1.77,1.33,1.54,1.62,0.55/)
     
-    diameter = (/0.16,0.45,0.17,0.25,0.34,0.4,0.23,0.49,0.37,0.5,0.53,0.12,0.75,0.22,0.63,0.31,0.41,0.63,0.52,0.15/)!,&
+    diameter = (/0.16,0.45,0.17,0.25,0.34/)!,0.4,0.23,0.49,0.37,0.5,0.53,0.12,0.75,0.22,0.63,0.31,0.41,0.63,0.52,0.15/)!,&
     ! !&0.16,0.45,0.17,0.25,0.34,0.4,0.23,0.49,0.37,0.5,0.53,0.12,0.75,0.22,0.63,0.31,0.41,0.63,0.52,0.15/)
 
     ! diameter = (/0.16,0.45,0.17,0.25,0.34,0.4,0.23,0.49,0.37,0.5,0.53,0.12,0.75,0.22,0.63,0.31,0.41,0.63,0.52,0.15,&
     ! &0.16,0.45,0.17,0.25,0.34,0.4,0.23,0.49,0.37,0.5,0.53,0.12,0.75,0.22,0.63,0.31,0.41,0.63,0.52,0.15/)
     
-    dens_1 = (/1.,2.,5.,3.3, 1.3, 7., 2.8, 3.,4.5,1.7,3.6,9.,4.,2.45,5.27,4.6,8.2,9.29,3.,4.8/)!,&
+    dens_1 = (/1.,2.,5.,3.3, 1.3/)!, 7., 2.8, 3.,4.5,1.7,3.6,9.,4.,2.45,5.27,4.6,8.2,9.29,3.,4.8/)!,&
     !&1.,2.,5.,3.3, 1.3, 7., 2.8, 3.,4.5,1.7,3.6,9.,4.,2.45,5.27,4.6,8.2,9.29,3.,4.8/)
 
     ! dens_1 = (/1.,2.,5.,3.3, 1.3, 7., 2.8, 3.,4.5,1.7,3.6,9.,4.,2.45,5.27,4.6,8.2,9.29,3.,4.8,&
     ! &1.,2.,5.,3.3, 1.3, 7., 2.8, 3.,4.5,1.7,3.6,9.,4.,2.45,5.27,4.6,8.2,9.29,3.,4.8/)
 
   
-
-    allocate (FPC_ind(1:npls))
-    allocate (FPC_pls_1(1:npls))
-    allocate (FPC_pls_2(1:npls))
-    allocate (FPC_pls(1:npls))
-    allocate (FPC_grid_t1(1:npls))
-    allocate (FPC_grid_t2(1:npls))
-    allocate (fpc_dec(1:npls))
-    allocate (fpc_dec_prop(1:npls))
-    allocate (FPC_inc(1:npls))
-    allocate (FPC_inc_cont(1:npls))
-    allocate (remaining(1:npls))
-    allocate (mort(1:npls))
-    allocate (mort_greff(1:npls))
-    allocate (greff(1:npls))
-    allocate (diam(1:npls))
-    allocate (crown_area(1:npls))
-    allocate (lai(1:npls))
-    allocate (carbon_increment(1:npls))
    
 
     ! ================= END VARIABLES DECLARATION ===================== !
@@ -693,6 +674,17 @@ program self_thinning
 
 
     enddo
+
+    open(unit=1,file='cleaf.csv',status='unknown')
+    do k=1, time
+        do j = 1,npls
+
+       
+            write(1,*) cl1_aux(j,k)/1000.,',','pls',j,',',k !newline
+        enddo
+    enddo    
+
+    close(1)
 
 
 end program self_thinning 
